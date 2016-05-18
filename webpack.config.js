f29c5e73fb63a51646f93804342bd3bde1ce80b2
yujilong <skyujilong@163.com>
2016-05-18 15:19:02 +0800

    add inline source map

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@
 var path = require("path");
 var webpack = require('webpack');
 module.exports = {
-    devtool: '#source-map',
+    devtool: '#inline-source-map',
     entry: {
         index:'index'
     },
